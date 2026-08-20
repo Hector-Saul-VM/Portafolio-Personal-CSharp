6839b52c9198a92e8d13767362d3e4a9ce7d0124
Héctor-Saul-VM <vegahector31@gmail.com>
2026-08-20 16:35:52 -0600

    feat: agregando primer ejercicio valor vrs referencia

---
+++ EjerciciosvalorVrsReferencia/ValorVrsReferencia/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ValorVrsReferencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VALOR 
+            int num1 = 5;
+            int num2 = num1; // se copia el valor
+
+            Console.WriteLine($"El valor del número 2 sin cambio es = {num2 }\n");
+
+            num2 = 10; // cambio num2, pero num1 no se ve afectado
+
+            Console.WriteLine("=== Tipos por valor ===");
+            Console.WriteLine($"num1 = {num1}"); // sigue siendo 5
+            Console.WriteLine($"num2 = {num2}"); // ahora es 10
+
+
+            // REFERENCIA 
+            int[] array1 = { 1, 2, 3 };
+            int[] array2 = array1; // se copia la referencia los 2 apuntan a la misma variable
+
+            array2[0] = 99; // cambio array2, pero también afecta a array1
+
+            Console.WriteLine("\n=== Tipos por referencia ===");
+            Console.WriteLine($"array1[0] = {array1[0]}");
+            Console.WriteLine($"array2[0] = {array2[0]}");
+        }
+    }
+}
